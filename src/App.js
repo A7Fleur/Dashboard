fff44b6962db083e09eb5b751252bce24a4b5983
A7Fleur <seitzhanovna@yandex.kz>
2023-01-21 00:26:33 +0600

    todays update

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ const App = () => {
   return (
     <div>
         <BrowserRouter>
-            <div className="sflex relative dark:bg-main-dark-bg">
+            <div className="flex relative dark:bg-main-dark-bg">
                 <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
                    <TooltipComponent content="Settings" position="Top">
-                    <button type="button" className="text-3xl p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white" style={{background: "blue", borderRadius: "50%" }}>
+                    <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: "blue", borderRadius: "50%" }}>
                       <FiSettings />
                     </button>
                    </TooltipComponent>
