b6b247892f294c07f2fc61eea29f9817e87ddbce
A7Fleur <seitzhanovna@yandex.kz>
2023-01-26 22:38:26 +0600

    finished the themeColor

--- src/App.js
+++ src/App.js
@@ -12,20 +12,28 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
         <BrowserRouter>
             <div className="flex relative dark:bg-main-dark-bg">
                 <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-                   <TooltipComponent content="Settings" position="Top">
-                    <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: "blue", borderRadius: "50%" }}>
-                      <FiSettings />
-                    </button>
-                   </TooltipComponent>
+                    <TooltipComponent 
+                      content="Settings" 
+                      position="Top"
+                    >
+                      <button 
+                        type="button" 
+                        className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                        onClick={() => setThemeSettings(true)} 
+                        style={{background: currentColor, borderRadius: "50%" }}
+                      >
+                        <FiSettings />
+                      </button>
+                    </TooltipComponent>
                 </div>
-                {activeMenu ?(
+                {activeMenu ? (
                   <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
                     <Sidebar />
                   </div>
@@ -35,13 +43,17 @@ const App = () => {
                   </div>
                 )}
                 <div className={
-                  `dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu ? "md:ml-72" : "flex-2"}` 
-                  }>
-                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                      <Navbar />
-                    </div>
+                  `dark:bg-main-dark-bg bg-main-bg min-h-screen  w-full 
+                  ${activeMenu ? "md:ml-72" : "flex-2"}` 
+                }>
+                  <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                    <Navbar />
+                  </div>
 
-                <div>
+                  <div>
+                   
+                  {themeSettings && <ThemeSettings />}
+
                   <Routes>
                     {/* Dashboard */}
                     <Route path='/' element={<Ecommerce />} /> 
@@ -68,7 +80,7 @@ const App = () => {
                     <Route path='/pyramid' element={<Pyramid />} /> 
                     <Route path='/stacked' element={<Stacked />} /> 
                   </Routes>
-                </div>
+                  </div>
                 </div>
             </div>
         </BrowserRouter>
